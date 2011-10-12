47beee3dd78ad058877434ef4bb09c7b0322ce39
Kinuko Yasuda <kinuko@chromium.org>
2011-10-12 15:15:34 +0900

    patch from issue 4969067

--- content.js
+++ content.js
@@ -1,156 +1,178 @@
-function __FileScope__() {
-    this.log = console.log.bind(console, 'FileScope:');
-
-    this.fs_init = function(fs) {
-        var self = __FileScope__main;
-        self.initialized = true;
-        self.fs = fs;
-        self.log('FileSystem Initialized.');
-        chrome.extension.sendRequest({'type' : 'init'});
-    }
-
-    this.fs_error = function(e) {
-        var self = __FileScope__main;
-        self.log('Error');
-        self.log(e);
-    }
-
-    this.fs_list = function() {
-        if (!this.reader)
-            this.reader = this.fs.root.createReader();
-            this.log('createReader');
-        if (this.busy) {
-            // TODO: Should be tested.
-            this.log('COVER: 1');
-            this.abort = true;
-            this.onabort = function() {
-                this.fs_list();
-            }
-            return;
-        }
-        if (this.abort) {
-            this.log('abort');
-            this.busy = false;
-            this.abort = false;
-            if (this.onabort) {
-                // TODO: Should be tested.
-                this.log('COVER: 2');
-                onabort = this.onabort;
-                this.onablort = null;
-                onabort();
-            }
-            return;
-        }
-        this.reader.readEntries(function(results) {
-                var self = __FileScope__main;
-                if (0 == results.length) {
-                    self.log('abort = true');
-                    self.abort = true;
-                } else {
-                    self.log('abort = false');
-                    for (var i = 0; i < results.length; i++) {
-                        if (i != (results.length - 1))
-                            self.send_entry(results[i], null);
-                        else
-                            self.send_entry(results[i], function() {
-                                    self.fs_list();
-                                });
-                    }
-                }
-            }, function(e) {
-                var self = __FileScope__main;
-                // TODO: Should be tested.
-                self.log('COVER: 3');
-                self.fs_error(e);
-                self.abort = true;
-                self.fs_list();
-            });
-    }
-
-    this.fs_chdir = function(dir) {
-        this.log('ChDir: ' + dir);
-        if (this.busy) {
-            this.log('...retry later');
-            this.abort = true;
-            this.onabort = function() {
-                this.fs_chdir(dir);
-            }
-            return;
-        }
-
-        this.busy = true;
-        this.fs.root.getDirectory(dir, {create: false}, function(entry) {
-                var self = __FileScope__main;
-                self.reader = entry.createReader();
-                self.busy = false;
-                self.abort = false;
-                self.fs_list();
-            }, function(e) {
-                var self = __FileScope__main;
-                // TODO: Should be tested.
-                self.log('COVER: 4');
-                self.fs_error(e);
-                self.abort = true;
-                self.fs_list();
-            });
-    }
-
-    this.send_entry = function(entry, callback) {
-        var type = entry.isFile ? 'file' : 'dir';
-        if (entry.isFile) {
-            entry.file(function(newFile) {
-                console.log('Send: ');
-                console.log('  type=' + type);
-                console.log('  name=' + entry.name);
-                console.log('  size=' + newFile.size);
-                console.log('  path=' + entry.fullPath);
-                console.log('  url=' + entry.toURL());
-                chrome.extension.sendRequest({
-                    'type': type,
-                    'name': entry.name,
-                    'size': newFile.size,
-                    'path': entry.fullPath,
-                    'url': entry.toURL()
-                });
-            });
-        }else {
-            console.log('Send: ');
-            console.log('  type=' + type);
-            console.log('  name=' + entry.name);
-            console.log('  path=' + entry.fullPath);
-            console.log('  url=' + entry.toURL());
-            chrome.extension.sendRequest({
-                'type': type,
-                'name': entry.name,
-                'path': entry.fullPath,
-                'url': entry.toURL(),
-                'files': null, //Array
-                'ul': null
-            });
-        }
-        if (callback)
-            callback();
-    }
-
-    this._fs_init = function(fs) {
-        __FileScope__main.fs_init(fs);
-    }
-
-    this._fs_error = function(e) {
-        __FileScope__main.fs_error(e);
-    }
-
-    this.busy = false;
-    this.abort = false;
-    this.onabort = null;
-    this.reader = null;
-    this.log('Loaded.');
-    // TODO: Choose PERSISTENT or TEMPORARY. HTML5Rocks uses TEMPORARY.
-    //window.webkitRequestFileSystem(window.PERSISTENT,
-    window.webkitRequestFileSystem(window.TEMPORARY,
-                                   1024 * 1024,
-                                   this._fs_init,
-                                   this._fs_error);
+function show_log(str) {
+    console.log.bind(console, 'Peephole:')(str);
 }
 
-var __FileScope__main = new __FileScope__();
+function fs_init(filesystem) {
+    fs = filesystem;
+    show_log('FileSystem Initialized.');
+    chrome.extension.sendRequest({'type' : 'init'});
+}
+
+function fs_error(e) {
+    show_log('Error');
+    show_log(e);
+    finish_request();
+}
+
+function fs_list(func) {
+    if (!busy) throw 'fs_list is called without lock.';
+    reader.readEntries(function(results) {
+        show_log(results);
+        if (results.length == 0) {
+            finish_request();
+            return;
+        }
+        currentPendingEntries = results.length;
+        for (var i = 0; i < results.length; i++) {
+            if (func)
+                func(results[i]);
+        }
+        fs_list(func);
+    }, function(e) {
+        fs_error(e);
+    });
+}
+
+function init_list() {
+    if (!lock()) return;
+
+    reader = fs.root.createReader();
+    show_log('createReader');
+    check_usage(fstype);
+
+    fs_list(send_entry);
+}
+
+function change_dir(dir) {
+    show_log('ChDir: ' + dir);
+    if (!lock()) return;
+
+    fs.root.getDirectory(
+        dir,
+        {create: false},
+        function(entry) {
+            reader = entry.createReader();
+            fs_list(send_entry);
+        }, function(e) {
+            fs_error(e);
+        });
+}
+
+function send_entry(entry) {
+    if (entry.isFile) {
+        entry.file(function(newFile) {
+            var request = {
+                'type': 'file',
+                'name': entry.name,
+                'size': newFile.size,
+                'path': entry.fullPath,
+                'url': entry.toURL()
+            };
+
+            show_log('currentPendingEntries = ' +
+                            currentPendingEntries);
+            show_log('Send: ', request);
+            chrome.extension.sendRequest(request);
+            check_send_all(entry);
+        }.bind(this));
+    } else {
+        var request = {
+            'type': 'dir',
+            'name': entry.name,
+            'path': entry.fullPath,
+            'url': entry.toURL()
+        };
+        show_log('currentPendingEntries = ' +
+                        currentPendingEntries);
+        show_log('Send: ', request);
+        chrome.extension.sendRequest(request);
+        check_send_all(entry);
+    }
+}
+
+function check_send_all(entry) {
+    if (--currentPendingEntries == 0) {
+        chrome.extension.sendRequest({
+            'path' : entry.fullPath,
+            'type' : 'show'
+        });
+    }
+}
+
+function change_type(type) {
+    if (!lock()) return;
+
+    finish_request();
+    reader = null;
+    fstype = type;
+
+    window.webkitRequestFileSystem(
+        fstype,
+        1024 * 1024,
+        fs_init,
+        fs_error
+    );
+}
+
+function delete_all() {
+    show_log('delete_all');
+    if (!lock()) return;
+
+    reader = fs.root.createReader();
+
+    fs_list(remove_file);
+}
+
+function remove_file(entry) {
+    show_log('remove entry');
+    if (entry.isFile) {
+        entry.remove(function() {
+            show_log('File Removed');
+        }, fs_error);
+    } else {
+        entry.removeRecursively(function() {
+            show_log('Dir Removed');
+        }, fs_error);
+    }
+}
+
+function check_usage(type) {
+    fstype = type;
+    show_log('fstype: ' + fstype);
+    webkitStorageInfo.queryUsageAndQuota(
+        fstype,
+        function(usage, quota) {
+            chrome.extension.sendRequest({
+            'type' : 'usage',
+            'usage': usage,
+            'quota': quota
+            });
+        }, fs_error);
+}
+
+function lock() {
+    if (busy)
+        throw 'Got request while handling another one';
+    busy = true;
+    return true;
+}
+
+function finish_request() {
+    busy = false;
+    chrome.extension.sendRequest({
+        'type' : 'finished',
+    });
+}
+
+var busy = false;
+var reader = null;
+var fstype = window.TEMPORARY;
+var fs = null;
+show_log('Loaded.');
+
+window.webkitRequestFileSystem(
+    fstype,
+    1024 * 1024,
+    fs_init,
+    fs_error
+);
--- popup.js
+++ popup.js
@@ -1,80 +1,260 @@
 function request_list(dir) {
-   chrome.tabs.executeScript(
-     null,
-     {code: '__FileScope__main.fs_chdir(\'' + dir + '\')'}
-   );
+    send_request('change_dir', dir + '/', null);
 }
 
 function command_init() {
-    chrome.tabs.executeScript(
-      null,
-      {code: '__FileScope__main.fs_list()'}
-    );
+    send_request('init_list', null, null);
 }
 
-function command_filedirCalled(request) {
-    var reqPath = request.path.split('/');
+function command_filedir(request) {
+    console.log('filedir');
+    var current = get_target_dir(request.path);
+    current.dir.push(request);
+}
 
-    console.log('reqPath: ', reqPath);
-
-    // Set up the 'li' node for the given new entry.
+function command_show(node, nodeul) {
+    // Set up the 'li' node for every entry.
     var li = document.createElement('li');
     var a = document.createElement('a');
-    li.classList.add(request.type);
-    li.setAttribute('title', request.url);
-    var text = document.createTextNode(request.name);
-    a.setAttribute('type', 'application/octet-stream');
-    a.appendChild(text);
+    li.setAttribute('class', node.type);
+    li.setAttribute('title', node.path);
+    var text = document.createTextNode(node.name);
+    var img = document.createElement('img'); // file dir icon
     li.appendChild(a);
 
-    if (request.type == 'dir') {
-        console.log('reqType is dir: ' + request.path);
-        request.files = new Array();
-        request.ul = document.createElement('ul');
-        li.appendChild(request.ul);
-        a.setAttribute('href', '#');
+    if (node.type == 'file') {
+        var divR = document.createElement('div');
+        divR.setAttribute('align', 'right');
+        divR.appendChild(document.createTextNode(get_unit(node.size)));
+        img.setAttribute('src', '/images/icon_file.png');
+        img.setAttribute('alt', '[file]');
+        a.appendChild(img);
+        a.appendChild(text);
+        a.appendChild(divR);
+        a.setAttribute('target', '_');
+        a.setAttribute('href', node.url);
+    } else {
+        // if the node is Dir, ask for the inside of the dir.
+        var img2 = document.createElement('img'); // open close icon
+        img2.setAttribute('src', '/images/icon_arrow_down.png');
+        img2.setAttribute('alt', 'Open');
+        node.ul = document.createElement('ul');
+        console.log('reqType is dir: ' + node.path);
+        li.appendChild(node.ul);
+        img.setAttribute('src', '/images/icon_folder.png');
+        img.setAttribute('alt', '[dir]');
         a.addEventListener('click', function() {
-            li.classList.add('open');
-            request_list(request.path);
+            if (busyCount == 0){
+                if (node.isOpened) { // CLOSING
+                    img2.setAttribute('src', '/images/icon_arrow_down.png');
+                    li.classList.remove('open');
+                    node.isOpened = false;
+                    li.removeChild(node.ul);
+                    node.ul = document.createElement('ul');
+                    li.appendChild(node.ul);
+                } else { // OPENING
+                    img2.setAttribute('src', '/images/icon_arrow_right.png');
+                    li.classList.add('open');
+                    node.files = [];
+                    node.isOpened = true;
+                    request_list(node.path);
+                }
+            }
+        });
+        a.appendChild(img2);
+        a.appendChild(img);
+        a.appendChild(text);
+        a.setAttribute('href', '#');
+    }
+    nodeul.appendChild(li);
+}
+
+function command_sort(dir) {
+    dir.sort(function(a, b) {
+        var nameA = a.name.toLowerCase();
+        var nameB = b.name.toLowerCase();
+        if (a.type != b.type) {
+            if (a.type == 'dir') return -1;
+            else return 1;
+        } else {
+            if (nameA < nameB) return -1;
+            else if (nameA > nameB) return 1;
+            else return 0;
+        }
+    });
+}
+
+function clear_field(){
+    rootdir = [];
+    div.removeChild(rootul);
+    rootul = document.createElement('ul');
+    div.appendChild(rootul);
+    usageDiv.removeChild(usageText);
+    usageText = document.createTextNode('');
+    usageDiv.appendChild(usageText);
+}
+
+function change_type(fstype) {
+    clear_field();
+    send_request('change_type', fstype, null);
+}
+
+function delete_all() {
+    var messagetext = 'Are you sure you want to ';
+    messagetext += 'delete ALL FILES and DIRECTORIES?';
+    if (confirm(messagetext)) {
+        console.log('eraseClicked');
+        send_request('delete_all', null, function(){
+            clear_field();
+            command_init();
+            alert('Content is Deleted.');
         });
     } else {
-        a.setAttribute('target', '_');
-        a.setAttribute('href', request.url);
+        alert('Deleting cancelled.');
     }
+}
 
-    // Look up where to add this entry.
+function get_unit(size) {
+    var unit = 0;
+    while (size > 1000 && unit < 5) {
+        size /= 1000;
+        unit++;
+    }
+    if (unit == 5) return 'unlimited';
+    size = Math.round(size * 100) / 100;
+    return size + ['', 'K', 'M', 'G', 'T'][unit] + 'B';
+}
+
+function busy_count_up() {
+    busyCount++;
+    header.removeChild(headText);
+    headText = document.createTextNode('Peephole (Processing...)');
+    header.appendChild(headText);
+}
+
+function busy_count_down() {
+    if (busyCount <= 0) {
+        console.log ('busy_count_down() is called while busyCount <= 0');
+        return;
+    }
+    
+    busyCount--;
+    if (busyCount == 0) {
+        header.removeChild(headText);
+        headText = document.createTextNode('Peephole');
+        header.appendChild(headText);
+        
+        if(finishedRequestCallback){
+            finishedRequestCallback();    
+        }
+    } else if (busyCount < 0) {
+        throw 'busyCount goes negative';
+    }
+}
+
+function get_target_dir(path) {
+    var splitted = path.split('/');
+
     var currentul = rootul;
     var currentdir = rootdir;
-    for (var i = 1; i < reqPath.length - 1; i++) {
+    for (var i = 1; i < splitted.length - 1; i++) {
         for (var j = 0; j < currentdir.length; j++) {
-            if (reqPath[i] == currentdir[j].name) {
+            if (splitted[i] == currentdir[j].name) {
                 currentul = currentdir[j].ul;
                 currentdir = currentdir[j].files;
                 break;
             }
         }
     }
-    console.log('currentdir:');
-    console.log(currentdir);
-
-    console.log('PUSH');
-    currentdir.push(request);
-    currentul.appendChild(li);
-
-    console.log('rootdir:');
-
+    return { dir: currentdir, ul: currentul};
 }
 
+function send_request(func, param, callbackfunc) {
+    console.log('send_request: ', func);
+    if (busyCount == 0) {
+        busy_count_up();
+        finishedRequestCallback = callbackfunc;
+        if (param){
+            chrome.tabs.executeScript(contentID, {
+                code: func + '(' + param + ')'
+            });
+        } else {
+            chrome.tabs.executeScript(contentID, {
+                code: func +'()'
+            });
+        }
+    }
+}
+
+// div for filelist
 var div = document.getElementById('file-list-body');
-var rootdir = new Array();
+var rootdir = []; // holds all the element (file/dir)
 var rootul = document.createElement('ul');
 div.appendChild(rootul);
+
+var busyCount = 0;
+var finishedRequestCallback = null;
+var contentID = 0;
+
+// div for storage size
+var usageDiv = document.getElementById('usage');
+var usageText = document.createTextNode('');
+
+// text for being busy
+var header = document.getElementById('header');
+var headText = document.createTextNode('Peephole');
+header.appendChild(headText);
+
+// set the Temporary/Persistent radio buttons
+var temp = document.getElementById('Temporary');
+var pers = document.getElementById('Persistent');
+temp.addEventListener('click', function() {
+    if (busyCount == 0) {
+        change_type(window.TEMPORARY);
+    }
+});
+pers.addEventListener('click', function() {
+    if (busyCount == 0) {
+        change_type(window.PERSISTENT);
+    }
+});
+
+// set the delete butten
+var deleteAll = document.getElementById('DeleteAll');
+deleteAll.addEventListener('click', delete_all);
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+        contentID = sender.tab.id;
         console.log(request);
-        if (request.type == 'init')
+        if (request.type == 'init') {
+            console.log('INITIATED1');
             command_init();
+        }
+        // show the size of storage use
+        else if (request.type == 'usage') {
+            console.log('currentUsageInBytes: ' + request.usage);
+            console.log('currentQuotaInBytes: ' + request.quota);
+
+            var leftSize = request.quota - request.usage;
+            usageText = document.createTextNode(
+                    'Size left: ' + get_unit(leftSize));
+            usageDiv.appendChild(usageText);
+        }
+        else if (request.type == 'show') {
+            var current = get_target_dir(request.path);
+
+            console.log('currentdir: ', current.dir);
+
+            command_sort(current.dir);
+
+            // show the contents of currentdir
+            for (var i = 0; i < current.dir.length; i++)
+                command_show(current.dir[i], current.ul);
+        }
+        else if (request.type == 'finished') {
+            busy_count_down();
+        }
         else
-            command_filedirCalled(request);
+            command_filedir(request);
     });
 chrome.tabs.executeScript(null, {file: 'content.js'});
-
